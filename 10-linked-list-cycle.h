caa48870a151082652d76521a2964db157992470
haohaosong <742398269@qq.com>
2017-04-26 10:03:30 +0800

    hhs:判断链表是否带环

---
+++ 10-linked-list-cycle.h
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* pFast = head;
+        ListNode* pSlow = head;
+        
+        while(pFast && pFast->next)
+        {
+            pFast = pFast->next->next;
+            pSlow = pSlow->next;
+            
+            if(pFast == pSlow)     
+                return true;
+        }
+        
+        return false;
+    }
+};
